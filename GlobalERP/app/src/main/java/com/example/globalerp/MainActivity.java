641454e7ed2d2641e44bb7919daff7b2a35576d7
AbhiChohan <30732573+super95@users.noreply.github.com>
2019-06-17 13:41:27 +0530

    Android app to access Global Institutes website and ERP
    Simple Android app to access Global Institutes website and ERP

---
+++ GlobalERP/app/src/main/java/com/example/globalerp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.globalerp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.ZwebView);
+        WebSettings myWebSettings = myWebView.getSettings();
+        myWebSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.globalinstitutes.edu.in/");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+//Developed BY ABHI CHOHAN super95.ab@gmail.com
